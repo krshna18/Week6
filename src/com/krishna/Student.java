86b57ef44c25c143667c16f37fa822d1d5d6c02a
Krishna_1809 <krishna.patel@alphatechsoftware.com>
2021-02-27 20:19:21 +0530

    Student class updatesd with field added

--- src/com/krishna/Student.java
+++ src/com/krishna/Student.java
@@ -1,9 +1,23 @@
 package com.krishna;
 
+import java.util.*;
+
 public class Student {
     int studentId;
     String firstName;
     String lastName;
+    Date birthDate;
+    Gender gender;
+    String city;
+    String pincode;
+    String cellPhone;
+    String email;
+    String department;
+    float marks[];
+    float totalMarks;
+    float percentage;
+    String grade;
+
 
 
 
