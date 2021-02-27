a401be4a61579303dafe1baf0aa9f8a727e27117
Krishna_1809 <krishna.patel@alphatechsoftware.com>
2021-02-27 20:30:12 +0530

    Added setter and getter in student class

--- src/com/krishna/Student.java
+++ src/com/krishna/Student.java
@@ -18,7 +18,134 @@ public class Student {
     float percentage;
     String grade;
 
+    public Student(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public Student(int studentId, String firstName, String lastName, Date birthDate, Gender gender, String city, String pincode, String cellPhone, String email, String department, float[] marks) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.city = city;
+        this.pincode = pincode;
+        this.cellPhone = cellPhone;
+        this.email = email;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
 
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public  String  getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public float[] getMarks() {
+        return marks;
+    }
+
+    public void setMarks(float[] marks) {
+        this.marks = marks;
+    }
+
+    public float getTotalMarks() {
+        return totalMarks;
+    }
+
+    private void setTotalMarks(float totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    private void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    private void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
